cf084543d0c3bdd08ec9a56f7f8b61d181f6a733
rsakie <rsakie@yahoo.com>
2017-06-28 15:03:35 -0400

    switch case practice

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,47 +10,67 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            //intro ex. thank you for dining with us..
-            Console.WriteLine("Thank you for Dining with us today");
+            ////intro ex. thank you for dining with us..
+            //Console.WriteLine("Thank you for Dining with us today");
 
-            Console.WriteLine("How many people are dining?");
-            int peopleDine = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How many people are dining?");
+            //int peopleDine = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("How much did each person pay each?");
-            int payEach = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How much did each person pay each?");
+            //int payEach = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Your total bill is.");
+            //Console.WriteLine("Your total bill is.");
 
-            double totalBill = peopleDine * payEach;
-            Console.WriteLine("$" + totalBill);
+            //double totalBill = peopleDine * payEach;
+            //Console.WriteLine("$" + totalBill);
 
-          
-            //total is above 50 add 10%
-            //below 50 add 5%
-            int total = 50;
-            if (total >= 50)
-            {
-                Console.WriteLine("You received a 10% discount!");
+
+            ////total is above 50 add 10%
+            ////below 50 add 5%
+            //int total = 50;
+            //if (total >= 50)
+            //{
+            //    Console.WriteLine("You received a 10% discount!");
+            //}
+
+            //    else
+            //{
+            //    Console.WriteLine("You Receive a 5% discount!");
+            //    //add discounts onto sub total of bill
+            //}
+            //{
+            //    double discount = totalBill * .10d;
+
+            //    Console.WriteLine( "$" + discount);
+
+            //    double grandTotal = totalBill - discount;
+            //    Console.WriteLine("Your Grand total with Discount is $" + grandTotal);
+
+            //}
+            //{
+            //    Console.WriteLine("Demographic Report based on age");
+
+            //    int age = int.Parse(Console.ReadLine());
+            //    if(age 0>= 
+
+
+            //string loyaltyLevel = "red";
+            //switch (loyaltyLevel)
+            //{
+            //    case "yellow":
+            //        Console.WriteLine("discount : 40%");
+            //        break;
+            //    case "red":
+            //        Console.WriteLine("discount: 90%");
+            //        break;
+            //    default:
+            //        Console.WriteLine("error");
+            //        break;
+
+
+
+
             }
-
-                else
-            {
-                Console.WriteLine("You Receive a 5% discount!");
-                //add discounts onto sub total of bill
-            }
-            {
-                double discount = totalBill * .10d;
-               
-                Console.WriteLine( "$" + discount);
-                
-                double grandTotal = totalBill - discount;
-                Console.WriteLine("Your Grand total with Discount is $" + grandTotal);
-                
-              
-
-              
-            }
-
             
 
 
